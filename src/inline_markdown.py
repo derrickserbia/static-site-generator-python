6c5e8e54ce67f0591b786a6336b256a44003dfed
derrickserbia <derrick.serbia@gmail.com>
2024-08-27 14:51:58 -0400

    rewriting code from boot.dev solution

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,9 +1,27 @@
-from textnode import *
 import re
+
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link,
+)
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-
     new_nodes = []
     for old_node in old_nodes:
         if old_node.text_type != text_type_text:
@@ -21,24 +39,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
-
     return new_nodes
 
 
-def extract_markdown_images(text):
-    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
-    return matches
-
-
-def extract_markdown_links(text):
-    matches = re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
-    return matches
-
-
-# [TextNode(
-#     "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
-#     text_type_text,
-# )]
 def split_nodes_image(old_nodes):
     new_nodes = []
     for old_node in old_nodes:
@@ -91,3 +94,15 @@ def split_nodes_link(old_nodes):
         if original_text != "":
             new_nodes.append(TextNode(original_text, text_type_text))
     return new_nodes
+
+
+def extract_markdown_images(text):
+    pattern = r"!\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    return matches
--- src/textnode.py
+++ src/textnode.py
@@ -5,7 +5,7 @@ text_type_code = "code"
 text_type_link = "link"
 text_type_image = "image"
 
-from htmlnode import LeafNode;
+from htmlnode import LeafNode
 
 
 class TextNode:
@@ -23,10 +23,11 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
-    
+
+
 def text_node_to_html_node(text_node):
 
-    if text_node.text_type == text_type_text: 
+    if text_node.text_type == text_type_text:
         return LeafNode(None, text_node.text)
     if text_node.text_type == text_type_bold:
         return LeafNode("b", text_node.text)
@@ -38,5 +39,11 @@ def text_node_to_html_node(text_node):
         return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type == text_type_image:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
-    
+
     raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+
+def text_to_textnodes(text):
+    textnodes = []
+
+    return textnodes
